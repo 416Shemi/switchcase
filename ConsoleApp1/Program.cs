721172f019944f7f99b06bdd7d35110b9806ef82
416Shemi <tu7ngcsvr@code.edu.az>
2022-03-04 09:08:15 +0400

    new proyekt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aylar = Console.ReadLine();
+            switch (aylar)
+            {
+                case "yanvar": Console.WriteLine(31);
+                    break;
+                case "fevral":Console.WriteLine(29);
+                    break;
+                case "mart":
+                    Console.WriteLine(31);
+                    break;
+                case "aprel":
+                    Console.WriteLine(30);
+                    break;
+                case "may":
+                    Console.WriteLine(31);
+                    break;
+                case "iyun":
+                    Console.WriteLine(30);
+                    break;
+                case "iyul":
+                    Console.WriteLine(31);
+                    break;
+                case "avqust":
+                    Console.WriteLine(30);
+                    break;
+                case "sentyabir":
+                    Console.WriteLine(31);
+                    break;
+                case "oktyabir":
+                    Console.WriteLine(30);
+                    break;
+                case "noyabir":
+                    Console.WriteLine(31);
+                    break;
+                case "dekabir":
+                    Console.WriteLine(30);
+                    break;
+                default:Console.WriteLine("ble bir ay yoxdur");
+                    break;
+
+
+
+
+
+            }
+            Console.Read();
+        }
+        
+    }
+}
